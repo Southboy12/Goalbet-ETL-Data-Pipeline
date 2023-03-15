66bd87a6e165e5277d8769d48f5149f90c2bd524
Southboy12 <menkachi85@gmail.com>
2023-03-15 12:43:20 +0100

    Added a line to convert data to csv file

--- extract_data.py
+++ extract_data.py
@@ -8,4 +8,5 @@ def extract_data():
         # I removed Time column because it's not in the last csv file
         df = pd.read_csv(link, usecols=['Div', 'Date', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG'])
         data.append(df)
-    return data
+    df = pd.concat(data, axis=0, ignore_index=True)
+    df.to_csv('data/league.csv', index=False)
